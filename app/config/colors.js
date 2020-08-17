8d380315a82145182e3202c3f85f0780bb3986f9
Megan Leaf <meganbleaf@gmail.com>
2020-08-17 05:52:28 -0400

    finished css

--- app/config/colors.js
+++ app/config/colors.js
@@ -1,9 +1,9 @@
 export default {
-    purple: '#540D6E',
-    blue: '#3A86FF',
-    pink: '#EE4266',
-    orange: '#FB5607',
-    green: '#337357',
-    teal: '#0EAD69',
+    purple: '#8338ec',
+    blue: '#49ADC1',
+    pink: '#E05265',
+    orange: '#F4A261',
+    green: '#56876D',
+    teal: '#157A6E',
     black: '#000'
 }
